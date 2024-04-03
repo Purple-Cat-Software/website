b3eb789fc9c6a775ffe2d3ec03cccb8143ac7a7b
Michiel <michieldesmet1994@gmail.com>
2024-04-03 16:18:54 +0200

    #0 - added dark/light theming

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+const onChangeTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
+</script>
+
 <template>
   <v-app-bar :elevation="2" rounded>
     <v-app-bar-title>Purple Cat Software</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-btn
+        :icon="theme.name.value === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+        @click='onChangeTheme'
+      />
+    </template>
   </v-app-bar>
 </template>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
-interface Project {
-  name: string;
-  subtitle: string;
-  description: string;
-  pictureUri: string;
-  linkUrl: string;
-}
+import { Project } from "../domain/Types";
 
 const projects: Project[] = [
   {
     name: "Purrfect Tool",
     subtitle: "Productivity tool with incorporated notes, task and timer",
     description: "'All-in-one' productivity tool, die de belangrijkste elementen samenvoegt in één simpele applicatie. Hierbij wil ik de gebruiker van alle gemakken voorzien en een ‘all-in-one oplossing’ aanbieden. Deze oplossing is een combinatie van notities, taken en een timer. Dit zijn de basisonderdelen die in versie 1.0 moeten komen. Daarna is het een constante doorontwikkeling van deze applicatie en deze verder uit te breiden.",
-    pictureUri: "@/assets/organise-purrfect-tool.svg",
     linkUrl: "https://google.com"
   }
 ];
@@ -31,20 +24,20 @@ const underConstruction = () => {
     v-for="project in projects"
     :key="project.name"
   >
-    <v-col cols="6">
+    <v-col cols="3">
       <v-img
         height="150"
         src="@/assets/organise-purrfect-tool.svg"
       />
     </v-col>
-    <v-col cols="6">
+    <v-col cols="9">
       <v-card
+        @click="underConstruction"
         append-icon="mdi-open-in-new"
         color="surface-variant"
         variant="text"
         :subtitle="project.subtitle"
         :title="project.name"
-        @click="underConstruction"
         target="_blank"
         rel="noopener noreferrer"
         rounded="lg"
